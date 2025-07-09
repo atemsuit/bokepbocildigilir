5643db690211d6a1d11299c592423f34ee8ce466
atemsuit <atemsuit@gmail.com>
2025-07-09 07:21:39 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://bocilsmpbali.pages.dev',
+  site: 'https://bokepbocildigilir.pages.dev',
   output: 'server',
   adapter: cloudflare(),
 });
